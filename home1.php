c135f790e1ef7e3b3ac8e9d58c8c541b25684cb1
210220015 <114793082+210220015@users.noreply.github.com>
2023-01-10 17:54:44 +0700

    Add files via upload

---
+++ home1.php
@@ -0,0 +1,18 @@
+<html>
+<html>
+<title>Menu></title>
+</head>
+<body>
+<center><h2>MENU MASTER<h/2></center>
+<center><a href="inputbarang.php">Menu Master Barang</a></center>
+<center><a href="inputpelanggan.php">Menu Master Pelanggan</a></center>
+<center><a href="inputkategori.php">Menu Master Kategori</a></center>
+<center><a href="inputtransaksi.php">Menu Master Transaksi</a></center>
+<center><h2>MENU TAMPIL<h/2></center>
+<center><a href="tampiltransaksi.php">Menu Tampil Transaksi</a></center>
+<center><a href="tampilbarang.php">Menu Tampil Barang</a></center>
+<center><a href="tampilpelanggan.php">Menu Tampil Pelanggan</a></center>
+<center><a href="tampilkategori.php">Menu Tampil Kategori</a></center>
+
+</body>
+	</html>
